b5f0fcba1e98ad2b2f0f37144f8f1f335fe69263
coumba <diagne@gmail.com>
2021-10-21 23:13:48 +0000

    Jpa8

---
+++ DemoJpa8Application.java
@@ -0,0 +1,13 @@
+package com.fatou.demoJpa8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJpa8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJpa8Application.class, args);
+	}
+
+}
